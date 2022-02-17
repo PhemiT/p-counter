1922289c72cf8f8f357303258da0cd5079f12b31
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-02-17 09:54:53 +0100

    add gitignore and some code

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // declaring variables 
+var saveEl = document.getElementById("save-el")
+var welcome = document.getElementById("welcome-message")
 var countEl = document.getElementById("count-el")
 var count = 0
+var greeting = "Welcome"
+var userName = "User"
+
+
+welcome.innerText = greeting + ", " + userName + "!"
+welcome.innerText += ":-)"
 
 // increment function 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
     return(count)
 }
 
+// reset fuction
 function reset() {
     count = 0
     countEl.innerText = count
     return(count)
+}
+
+// save function 
+function save() {
+    var countStr = count + " - "
+    saveEl.innerText += countStr
 }
